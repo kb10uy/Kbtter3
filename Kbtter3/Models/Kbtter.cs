292d56f2c641ca722e69349eecbc21f9058a592b
kb10uy <kb10uy@live.jp>
2014-05-03 12:51:15 +0900

    Kbtter3として初版公開 改めてKbtter3をよろしくお願いします

---
+++ Kbtter3/Models/Kbtter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CoreTweet;
+using CoreTweet.Core;
+using CoreTweet.Rest;
+using CoreTweet.Streaming;
+using CoreTweet.Streaming.Reactive;
+
+using Livet;
+
+namespace Kbtter3.Models
+{
+    public class Kbtter : NotificationObject
+    {
+        /*
+         * NotificationObjectはプロパティ変更通知の仕組みを実装したオブジェクトです。
+         */
+    }
+}
