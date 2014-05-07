5aced3154308785b405c8e95ddf78f32d900febb
kb10uy <kb10uy@live.jp>
2014-05-07 18:50:58 +0900

    ModelのKbtter.csをちょっと書いた あとCoreTweetの更新

--- Kbtter3/Models/Kbtter.cs
+++ Kbtter3/Models/Kbtter.cs
@@ -25,16 +25,48 @@ namespace Kbtter3.Models
 
         public IDisposable Stream { get; protected set; }
 
+        public List<Status> Cache { get; set; }
+
+        public event Action<StatusMessage> OnStatus;
+        public event Action<EventMessage> OnEvent;
+
+        public StatusMessage LatestStatus { get; set; }
+        public EventMessage LatestEvent { get; set; }
+
         public void Initialize(string at, string ats)
         {
             Token = Tokens.Create("", "", at, ats);
+            Cache = new List<Status>();
         }
 
         public void StartStreaming()
         {
-            var ob = Token.Streaming.StartObservableStream(StreamingType.User).Publish();
-
+            var ob = Token.Streaming.StartObservableStream(StreamingType.User)
+                .Publish();
+            ob.OfType<StatusMessage>().Subscribe((p) =>
+            {
+                if (OnStatus != null) OnStatus(p);
+            });
+            ob.OfType<EventMessage>().Subscribe((p) =>
+            {
+                if (OnEvent != null) OnEvent(p);
+            });
             Stream = ob.Connect();
+
+            OnStatus += NotifyStatusUpdate;
+            OnEvent += NotifyEventUpdate;
+        }
+
+        void NotifyStatusUpdate(StatusMessage msg)
+        {
+            LatestStatus = msg;
+            RaisePropertyChanged(() => LatestStatus);
+        }
+
+        void NotifyEventUpdate(EventMessage msg)
+        {
+            LatestEvent = msg;
+            RaisePropertyChanged(()=>LatestEvent);
         }
 
         public void StopStreaming()
